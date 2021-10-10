1f52f4cebe7896e62b5db5fdd9e1b749aed79134
AnatoliyNBulyga <anatoliynbulyga@gmail.com>
2021-10-10 15:43:22 +0300

    init start template

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>My blog</title>
+      </Head>
+
+      <main>
+
+      </main>
+
+    </div>
+  )
+}
+
+export default Home
